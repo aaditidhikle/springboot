c01a8a4c7fd48b8105a88a44cb8a465bd31f52c8
aaditidhikle <aditidhikle@gmail.com>
2022-06-22 19:08:10 -0700

    Update CollaborativeFilter.java

--- CollaborativeFilter.java
+++ CollaborativeFilter.java
@@ -1,7 +1,7 @@
-public class CollaborativeFilter {
+public class CollaborativeFilter implements Filter{
  
     public String[] getRecommendations(String movie) {
         //logic of collaborative filter
-        return new String[] { };
+        return new String[] { "Finding Nemo", "Ice Age", "Toy Story" };
      }
  }
